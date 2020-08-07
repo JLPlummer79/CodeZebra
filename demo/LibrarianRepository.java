29ea37dac3c92f62bbbb9c24bd8ee0c26d4b310a
Jesse <bfantastic@gmail.com>
2020-08-07 11:46:49 -0700

    "Uploaded project incorrectly"

--- demo/LibrarianRepository.java
+++
@@ -1,9 +0,0 @@
-package com.librarydata.demo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibrarianRepository extends CrudRepository<Librarian, Long> {
-
-}
