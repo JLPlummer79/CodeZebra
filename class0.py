85a250de1ab06ee1fab447223fa095d7f91b1d37
Jesse <bfantastic@gmail.com>
2021-05-31 08:41:46 -0700

    "Practicex3"

--- class0.py
+++ class0.py
@@ -13,11 +13,19 @@ class PartyAnimal:
     def __del__(self):
         print('I am death the destroyer of objects', self.name)
 
-q= PartyAnimal('Quincy')
-m = PartyAnimal('Miya')
+
+class FootballFan(PartyAnimal):
+    points = 0
+    def touchdown(self):
+        self.points = self.points + 7
+        self.party()
+        print(self.name, "points", self.points)
 
 
-q.party()
-m.party()
-q.party()
+s = PartyAnimal("Sally")
+s.party()
+
+j = FootballFan("Jim")
+j.party()
+j.touchdown()
 
